4b4187599ae490997f89a4453920e839a5bfa8cc
Mariana <mntaba11@gmail.com>
2024-03-28 07:55:12 -0300

    ok

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -19,70 +19,70 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(50, 100, 50, 100),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              SizedBox(height: 30),
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(50, 100, 50, 100),
+            child: Form(
+                key: formKey,
+                child: Column(children: [
+                  SizedBox(height: 30),
 
-              TextFormField(
-                controller: txtValor1,
-                style: TextStyle(fontSize: 32),
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                ),
-                //
-                // VALIDAÇÃO
-                //
-                validator: (value) {
-                  if (value == null) {
-                    return 'Informe o email';
-                  } else if (value.isEmpty) {
-                    return 'Informe o email';
-                  } else if (double.tryParse(value) == null) {
-                    return 'Informe um valor NUMÉRICO';
-                  }
-                  //Retornar null significa sucesso na validação
-                  return null;
-                },
-              ),
-              SizedBox(height: 30),
-              TextFormField(
-                controller: txtValor2,
-                style: TextStyle(fontSize: 32),
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null) {
-                    return 'Informe sua senha';
-                  } else if (value.isEmpty) {
-                    return 'Informe sua senha';
-                  } else if (double.tryParse(value) == null) {
-                    return 'Informe um valor NUMÉRICO';
-                  }
-                  //Retornar null significa sucesso na validação
-                  return null;
-                },
-              ),
-              SizedBox(height: 30),
-              //
-              // BOTÃO
-              //
-              //ElevatedButton, OutlinedButton, TextButton
-              OutlinedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(200, 50),
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(
-                  context,
-                  'cadastro',
-                );
-              );}    );
-                    };
-                  }
+                  TextFormField(
+                    controller: txtValor1,
+                    style: TextStyle(fontSize: 32),
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    //
+                    // VALIDAÇÃO
+                    //
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Informe o email';
+                      } else if (value.isEmpty) {
+                        return 'Informe o email';
+                      } else if (double.tryParse(value) == null) {
+                        return 'Informe um valor NUMÉRICO';
+                      }
+                      //Retornar null significa sucesso na validação
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 30),
+                  TextFormField(
+                    controller: txtValor2,
+                    style: TextStyle(fontSize: 32),
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Informe sua senha';
+                      } else if (value.isEmpty) {
+                        return 'Informe sua senha';
+                      } else if (double.tryParse(value) == null) {
+                        return 'Informe um valor NUMÉRICO';
+                      }
+                      //Retornar null significa sucesso na validação
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 30),
+                  //
+                  // BOTÃO
+                  //
+                  //ElevatedButton, OutlinedButton, TextButton
+                  OutlinedButton(
+                    child: {
+
+                    },
+                      onPressed: () {
+                        Navigator.pushNamed(
+                          context,
+                          'cadastro',
+                        );
+                      })
+                ]))));
+  }
+}
