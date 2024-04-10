b54981c75923032683eae84c6c759c340e8bedf6
Mariana <mntaba11@gmail.com>
2024-04-09 23:57:59 -0300

    we

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:lista_compras/view/sobre_view.dart';
 import 'view/cadastro_view.dart';
 import 'view/lista_view.dart';
 import 'view/login_view.dart';
+import 'view/lc1.dart';
 
 
 
@@ -34,6 +35,7 @@ class MainApp extends StatelessWidget {
         'esquecer_senha':(context) => EsquecerSenhaView(),
         'sobre':(context) => SobreView(),
         'lista':(context) => ListaView(),
+        'lc1': (context) => Lc1View(),
 
       },
     );
--- lib/view/lista_view.dart
+++ lib/view/lista_view.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import '../model/compras.dart';
 
 class ListaView extends StatefulWidget {
   const ListaView({super.key});
@@ -11,30 +13,46 @@ class ListaView extends StatefulWidget {
 }
 
 class _ListaViewState extends State<ListaView> {
+
+  List<Compras> dados = [];
+
   @override
+  void initState() {
+    dados = Compras.preencher();
+    super.initState();
+  }
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 200,
-                child: GridView.count(
-                  scrollDirection: Axis.horizontal,
-                  crossAxisCount: 2,
-                  children: List.generate(50, (index) {
-                    return Container(
-                      child: Card(
-                        color: Colors.amber,
-                      ),
-                    );
-                  }),
-                ),
-              )
-            ],
-          ),
+      appBar: AppBar(
+        title: Text('Compras', style: TextStyle(color: Colors.yellow,),),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: dados.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                title: Text(dados[index].titulo),
+
+                onTap: (){
+                  Navigator.pushNamed(context, 'lc1');
+                },
+                hoverColor: Colors.red.shade100,
+
+                onLongPress: (){
+                  setState(() {
+                    dados.removeAt(index);
+                  });
+                },
+
+              ),
+            );
+          },
         ),
+
       ),
     );
   }
